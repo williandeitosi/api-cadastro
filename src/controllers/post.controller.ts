3d49cbc84dd9bea0768972292bb639f34d0762e3
williandeitosi <williandeitosi@gmail.com>
2024-09-09 22:10:21 -0300

    fix: `compose.yml`, start `post.controller.ts`

---
+++ src/controllers/post.controller.ts
@@ -0,0 +1,5 @@
+import type { Request, Response } from 'express'
+
+export const getAllPosts = async (req: Request, res: Response) => {
+  // TODO: change name function for get post user, list user with post, not list all posts
+}
